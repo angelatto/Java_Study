3afd98628643a31380d22094696c2556e8d8c401
angelatto <pudri109@gmail.com>
2021-02-19 09:05:07 +0900

    [add] java ch04 homework code

---
+++ src/ch04homework/SwitchStringExample.java
@@ -0,0 +1,21 @@
+package ch04homework;
+
+//public class SwitchStringExample {
+//
+//	public static void main(String[] args) {
+//		String position = "����";
+//		
+//		switch(position) {
+//			case "����":
+//				System.out.println("700����");
+//				break;
+//			 case "����":
+//				System.out.println("500����");
+//				break;
+//			 default:
+//				System.out.println("300����");
+//		}
+//
+//	}
+//
+//}
