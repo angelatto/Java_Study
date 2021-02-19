bcf01abb6165b1c5ffcc9bb00a5452c1bf3f0796
angelatto <pudri109@gmail.com>
2021-02-19 20:19:19 +0900

    [add] study java chapter05 book code

---
+++ src/pr05/LoginResult.java
@@ -0,0 +1,7 @@
+package pr05;
+
+public enum LoginResult {
+	SUCCESS,
+	FAILURE_ID,
+	FAILURE_PW
+}
--- src/pr05/exam04.java
+++ src/pr05/exam04.java
@@ -6,22 +6,22 @@ public class exam04 {
 		// TODO Auto-generated method stub
 		int[] arr1 = new int[3];
 		for(int i=0; i<arr1.length; i++) {
-			System.out.println(arr1[i]);
+			System.out.println(arr1[i]); // 0 
 		}
 		
 		double[] arr2 = new double[3];
 		for(int i=0; i<arr2.length; i++) {
-			System.out.println(arr2[i]);
+			System.out.println(arr2[i]); // 0.0 
 		}
 		
 		boolean[] arr3 = new boolean[3];
 		for(int i=0; i<arr3.length; i++) {
-			System.out.println(arr3[i]);
+			System.out.println(arr3[i]); // false
 		}
 		
 		String[] arr4 = new String[3];
 		for(int i=0; i<arr4.length; i++) {
-			System.out.println(arr4[i]);
+			System.out.println(arr4[i]); // null
 		}
 	}
 
