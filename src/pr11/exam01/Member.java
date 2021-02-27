0cfe1dd807ca521fa3c6851edefa33491a82a79b
angelatto <pudri109@gmail.com>
2021-02-27 20:26:43 +0900

    [add] Java chapter11 exam01 study

---
+++ src/pr11/exam01/Member.java
@@ -0,0 +1,30 @@
+package pr11.exam01;
+
+public class Member {
+
+	private String id;
+	private String name;
+	
+	
+	public Member(String id, String name) {
+		super(); // 생략 가능. 컴파일러가 자동적으로 붙여줌. 
+		this.id = id;
+		this.name = name;
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member m = (Member) obj;
+			if(id.equals(m.id) && name.equals(m.name)) {
+				return true;
+			}else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
+}
